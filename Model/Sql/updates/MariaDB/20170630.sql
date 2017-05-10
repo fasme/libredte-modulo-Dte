887fd5b3a14b609bea46a4b65b90e9a1fd683b59
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-05-10 12:37:19 -0300

    fix track_id de INTEGER a BIGINT

---
+++ Model/Sql/updates/MariaDB/20170630.sql
@@ -0,0 +1,17 @@
+--
+-- Actualización al día 30 de junio de 2017
+--
+
+START TRANSACTION;
+
+ALTER TABLE dte_intercambio_recepcion MODIFY glosa VARCHAR(256) NULL;
+
+-- fix track_id de INTEGER a BIGINT
+ALTER TABLE dte_emitido MODIFY track_id BIGINT;
+ALTER TABLE dte_emitido MODIFY cesion_track_id BIGINT;
+ALTER TABLE dte_venta MODIFY track_id BIGINT;
+ALTER TABLE dte_compra MODIFY track_id BIGINT;
+ALTER TABLE dte_guia MODIFY track_id BIGINT;
+ALTER TABLE dte_boleta_consumo MODIFY track_id BIGINT;
+
+COMMIT;
--- Model/Sql/updates/PostgreSQL/20170630.sql
+++ Model/Sql/updates/PostgreSQL/20170630.sql
@@ -6,4 +6,12 @@ BEGIN;
 
 ALTER TABLE dte_intercambio_recepcion ALTER glosa DROP NOT NULL;
 
+-- fix track_id de INTEGER a BIGINT
+ALTER TABLE dte_emitido ALTER track_id TYPE BIGINT;
+ALTER TABLE dte_emitido ALTER cesion_track_id TYPE BIGINT;
+ALTER TABLE dte_venta ALTER track_id TYPE BIGINT;
+ALTER TABLE dte_compra ALTER track_id TYPE BIGINT;
+ALTER TABLE dte_guia ALTER track_id TYPE BIGINT;
+ALTER TABLE dte_boleta_consumo ALTER track_id TYPE BIGINT;
+
 COMMIT;
