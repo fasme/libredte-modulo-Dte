51b590944f6d8a3040fb042c640e6444a61d3da0
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-05-18 22:30:20 -0400

    Se escapan argumentos de la shell

--- Controller/Documentos.php
+++ Controller/Documentos.php
@@ -961,7 +961,7 @@ class Controller_Documentos extends \Controller_App
      * Acción que permite generar masivamente los DTE
      * En estrictor rigor esta opción sólo lanza un comando que permite hacer la generación masiva
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2018-04-28
+     * @version 2018-05-18
      */
     public function emitir_masivo()
     {
@@ -976,7 +976,7 @@ class Controller_Documentos extends \Controller_App
             }
             $archivo = tempnam('/tmp', $Emisor->rut.'_dte_masivo_pendiente_');
             move_uploaded_file($_FILES['archivo']['tmp_name'], $archivo);
-            $cmd = 'Dte.Documentos_EmitirMasivo '.(int)$Emisor->rut.' '.$archivo.' '.(int)$this->Auth->User->id.' '.(int)$_POST['dte_emitido'].' '.(int)$_POST['email'].' -v';
+            $cmd = 'Dte.Documentos_EmitirMasivo '.escapeshellcmd((int)$Emisor->rut).' '.$archivo.' '.escapeshellcmd((int)$this->Auth->User->id).' '.escapeshellcmd((int)$_POST['dte_emitido']).' '.escapeshellcmd((int)$_POST['email']).' -v';
             $log = TMP.'/screen_documentos_emitir_masivo_'.$Emisor->rut.'_'.date('YmdHis').'.log';
             if ($this->shell($cmd, $log)) {
                 \sowerphp\core\Model_Datasource_Session::message(
