8d292971f615ba4a66e3ddefbb9eed23ee7eb376
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-10-22 22:38:49 -0300

    Se quita icono imprimir del vale de cotizacion

--- View/DteTmps/vale.php
+++ View/DteTmps/vale.php
@@ -3,5 +3,5 @@
     <span class="lead">Total $<?=num($DteTmp->total)?></span><br/><br/>
     <img src="<?=$_base?>/exportar/barcode/<?=base64_encode($DteTmp->getFolio())?>" alt="Barcode <?=$DteTmp->getFolio()?>" style="width:60mm" /><br/>
     <?=$DteTmp->getTipo()->tipo?>: <?=$DteTmp->getFolio()?><br/>
-    <a href="javascript:window.print()" class="small hidden-print"><span class="fa fa-print"></span> imprimir</a>
+    <a href="javascript:window.print()" class="small d-print-none"><span class="fa fa-print"></span> imprimir</a>
 </div>
