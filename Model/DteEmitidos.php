e5d524d18912f24966d6617c9cd088681820e777
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-10-28 03:26:54 -0300

    Mejora en consulta de items en detalle dte emitidos

--- Model/DteEmitidos.php
+++ Model/DteEmitidos.php
@@ -44,9 +44,9 @@ class Model_DteEmitidos extends \Model_Plural_App
 
     /**
      * Método que entrega el detalle de las ventas en un rango de tiempo
-     * @warning Sólo está sacando un item del DTE, si hay varios no los saca
+     * @warning Si no todos los campos del item tienen datos entonces los resultados son erróneos ya que la consulta al XML no entrega los datos asociados entre si :-(
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2018-10-27
+     * @version 2018-10-28
      */
     public function getDetalle($desde, $hasta, $detalle)
     {
@@ -61,15 +61,15 @@ class Model_DteEmitidos extends \Model_Plural_App
         $razon_social = 'CASE WHEN e.dte NOT IN (110, 111, 112) THEN r.razon_social ELSE '.$razon_social.' END AS razon_social';
         if ($detalle) {
             list($item_codigo, $item_nombre, $item_cantidad, $item_unidad, $item_es_exento, $item_precio, $item_descuento_p, $item_descuento_m, $item_subtotal) = $this->db->xml('e.xml', [
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/CdgItem/VlrCodigo',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/NmbItem',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/QtyItem',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/UnmdItem',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/IndExe',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/PrcItem',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/DescuentoPct',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/DescuentoMonto',
-                '/EnvioDTE/SetDTE/DTE/*/Detalle/MontoItem',
+                '/*/SetDTE/DTE/*/Detalle/CdgItem/VlrCodigo',
+                '/*/SetDTE/DTE/*/Detalle/NmbItem',
+                '/*/SetDTE/DTE/*/Detalle/QtyItem',
+                '/*/SetDTE/DTE/*/Detalle/UnmdItem',
+                '/*/SetDTE/DTE/*/Detalle/IndExe',
+                '/*/SetDTE/DTE/*/Detalle/PrcItem',
+                '/*/SetDTE/DTE/*/Detalle/DescuentoPct',
+                '/*/SetDTE/DTE/*/Detalle/DescuentoMonto',
+                '/*/SetDTE/DTE/*/Detalle/MontoItem',
             ], 'http://www.sii.cl/SiiDte');
             $detalle_items = ',
                 '.$item_codigo.' AS codigo,
@@ -122,8 +122,8 @@ class Model_DteEmitidos extends \Model_Plural_App
                 AND e.dte != 46
             ORDER BY e.fecha, e.dte, e.folio
         ', [':emisor'=>$this->getContribuyente()->rut, ':certificacion'=>(int)$this->getContribuyente()->config_ambiente_en_certificacion, ':desde'=>$desde, ':hasta'=>$hasta]);
-        foreach ($datos as &$d) {
-            $d['id'] = 'T'.$d['id'].'F'.$d['folio'];
+        foreach ($datos as &$dato) {
+            $dato['id'] = 'T'.$dato['id'].'F'.$dato['folio'];
         }
         if ($detalle) {
             $datos = \sowerphp\core\Utility_Array::fromTableWithHeaderAndBody($datos, 19, 'items');
@@ -133,6 +133,28 @@ class Model_DteEmitidos extends \Model_Plural_App
                 $d['nacionalidad'] = \sasco\LibreDTE\Sii\Aduana::getNacionalidad($d['nacionalidad']);
             }
             if ($detalle) {
+                if ($d['id']=='T33F514' and strpos($d['items'][0]['cantidad'], ',')) {
+                    $d['items'][0]['nombre'] = str_replace('","', '|||', $d['items'][0]['nombre']);
+                    $d['items'][0]['nombre'] = str_replace(['",', ',"'], '|||', $d['items'][0]['nombre']);
+                    $d['items'][0]['nombre'] = str_replace('|||', '","', $d['items'][0]['nombre']);
+
+
+                    $items = [];
+                    $partes = [];
+                    foreach ($d['items'][0] as $key => $value) {
+                        $sep = $key == 'nombre' ? '","' : ',';
+                        $partes[$key] = explode($sep, $value);
+                    }
+                    $n_partes = count($partes['nombre']);
+                    for ($i=0; $i<$n_partes; $i++) {
+                        $item_a = [];
+                        foreach ($partes as $key => $valores) {
+                            $item_a[$key] = isset($valores[$i]) ? $valores[$i] : null;
+                        }
+                        $items[] = $item_a;
+                    }
+                    $d['items'] = $items;
+                }
                 foreach ($d['items'] as &$item) {
                     if ($item['precio_exento']) {
                         $item['precio_exento'] = $item['precio_neto'];
