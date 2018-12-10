db27e13e55b48cf7202089c5aa6c188d5aa381ed
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-12-10 03:49:19 -0300

    Si el rechazo es por error de firma se permite reenviar el DTE al SII (podría ser firma no autorizada previamente y con esto se logra enviar)

--- View/DteEmitidos/ver.php
+++ View/DteEmitidos/ver.php
@@ -121,6 +121,10 @@ new \sowerphp\general\View_Helper_Table([
                             <a href="#" onclick="__.popup('<?=$_base?>/dte/sii/estado_envio/<?=$DteEmitido->track_id?>', 750, 550)" title="Ver el estado del envío en la web del SII">ver estado envío en SII</a><br/>
 <?php endif; ?>
                             <a href="#" onclick="__.popup('<?=$_base?>/dte/sii/verificar_datos/<?=$DteEmitido->getReceptor()->getRUT()?>/<?=$DteEmitido->dte?>/<?=$DteEmitido->folio?>/<?=$DteEmitido->fecha?>/<?=$DteEmitido->getTotal()?>', 750, 550)" title="Verificar datos del documento en la web del SII">verificar documento en SII</a>
+<?php if (substr($DteEmitido->revision_estado,0,3)=='RFR') : ?>
+                            <br/>
+                            <a href="<?=$_base?>/dte/dte_emitidos/enviar_sii/<?=$DteEmitido->dte?>/<?=$DteEmitido->folio?>" onclick="return Form.checkSend('¿Confirmar el reenvío del DTE al SII?')">reenviar DTE al SII</a>
+<?php endif; ?>
 <?php if ($DteEmitido->getEstado()=='R' or $DteEmitido->track_id==-1) : ?>
                             <br/>
                             <a href="<?=$_base?>/dte/dte_emitidos/eliminar/<?=$DteEmitido->dte?>/<?=$DteEmitido->folio?>" title="Eliminar documento" onclick="return Form.checkSend('¿Confirmar la eliminación del DTE?')">eliminar documento</a>
