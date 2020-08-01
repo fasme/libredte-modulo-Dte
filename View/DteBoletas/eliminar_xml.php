7af8687ba6c03c0c7e15619c65bf7289681d64ad
Esteban De La Fuente Rubio <esteban@delaf.cl>
2020-08-01 15:39:05 -0400

    Se agrega 2da confirmación

--- View/DteBoletas/eliminar_xml.php
+++ View/DteBoletas/eliminar_xml.php
@@ -11,7 +11,7 @@
     <div class="col-sm-8">
 <?php
 $f = new \sowerphp\general\View_Helper_Form();
-echo $f->begin(['onsubmit'=>'Form.check()']);
+echo $f->begin(['onsubmit'=>'Form.check() && Form.confirm(this, \'Por favor confirme que desea eliminar los XML<br/><br/><strong>No podrá recuperarlos desde LibreDTE si los borra</strong>\')']);
 echo $f->input([
     'name' => 'periodo',
     'label' => 'Período',
